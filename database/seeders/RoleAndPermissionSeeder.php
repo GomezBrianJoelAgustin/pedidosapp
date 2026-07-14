d383cfa19b0cba34f4c3d7ace999c243a8de0640
Agustin Gomez <164429301+GomezBrianJoelAgustin@users.noreply.github.com>
2026-07-14 20:10:04 -0300

    new: Roles and permissions created and assigned successfully

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 
 class RoleAndPermissionSeeder extends Seeder
 {
@@ -12,6 +15,73 @@ class RoleAndPermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'view products']);
+        Permission::create(['name' => 'create products']);
+        Permission::create(['name' => 'edit products']);
+        Permission::create(['name' => 'delete products']);
+
+        Permission::create(['name' => 'view orders']);
+        Permission::create(['name' => 'create orders']);
+        Permission::create(['name' => 'edit orders']);
+        Permission::create(['name' => 'delete orders']);
+        Permission::create(['name' => 'update order status']);
+        Permission::create(['name' => 'cancel order']);
+        Permission::create(['name' => 'view my orders']);
+
+        Permission::create(['name' => 'view users']);
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'edit users']);
+        Permission::create(['name' => 'delete users']);
+
+        Permission::create(['name' => 'open box']);
+        Permission::create(['name' => 'close box']);
+
+        $superAdminRole = Role::create(['name' => 'super-admin']);
+        $adminRole = Role::create(['name' => 'admin']);
+        $cashierRole = Role::create(['name' => 'cashier']);
+        $chefRole = Role::create(['name' => 'chef']);
+        $deliveryRole = Role::create(['name' => 'delivery']);
+        $clienteRole = Role::create(['name' => 'client']);
+
+        $superAdminRole->givePermissionTo(Permission::all());
+        $adminRole->givePermissionTo([
+            'view products',
+            'create products',
+            'edit products',
+            'delete products',
+            'view orders',
+            'create orders',
+            'edit orders',
+            'delete orders',
+            'update order status',
+            'view users',
+            'create users',
+            'edit users',
+            'delete users',
+        ]);
+        $cashierRole->givePermissionTo([
+            'create orders',
+            'edit orders',
+            'delete orders',
+            'update order status',
+        ]);
+        $chefRole->givePermissionTo([
+            'view orders',
+            'update order status',
+        ]);
+        $deliveryRole->givePermissionTo([
+            'view orders',
+            'update order status',
+        ]);
+        $clienteRole->givePermissionTo([
+            'create orders',
+            'view my orders',
+            'cancel order',
+            'edit users',
+            'edit orders',
+            'view products'
+        ]);
     }
 }
