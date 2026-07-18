b92c8a954f89a3fbbe578c1aaf906c34737803de
Agustin Gomez <164429301+GomezBrianJoelAgustin@users.noreply.github.com>
2026-07-18 14:28:20 -0300

    feat: create a models and migrations for the sale and inventory sistem

---
+++ database/migrations/2026_07_18_164418_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('delivery_id')->constrained('users')->onDelete('cascade');
+            $table->string('status')->default('pending');
+            $table->string('delivery_type')->default('delivery');
+            $table->string('delivery_address')->nullable();
+            $table->string('payment_method');
+            $table->string('payment_status')->default('pending');
+            $table->string('payment_gateway_id')->nullable();
+            $table->decimal('total_price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
